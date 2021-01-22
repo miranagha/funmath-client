7232814b19576d68489e424fe5b4f0dc1ed0104d
Miran Agha <magha001@gold.ac.uk>
2021-01-22 04:31:09 +0000

    footer changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer mt-auto py-3 bg-dark d-print-none">
       <Container className="d-flex justify-content-between text-center text-light">
         <span>
-            Fun Math App
+            Fun Math App by Miran
         </span>
       </Container>
     </footer>
